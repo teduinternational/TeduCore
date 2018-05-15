6b56b8591e1b37f58cd6199cb6517a6622cedd72
Toanbn <ngoctoan89@gmail.com>
2018-05-15 08:16:15 +0700

    save account module

--- adminapp/src/account/account.module.ts
+++ adminapp/src/account/account.module.ts
@@ -3,11 +3,8 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule, JsonpModule } from '@angular/http';
 import { ModalModule } from 'ngx-bootstrap/modal';
-
 import { AccountRoutingModule } from './account-routing.module';
-
 import { AccountComponent } from './account.component';
-
 import { LoginComponent } from './login/login.component';
 
 @NgModule({
